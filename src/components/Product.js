cf2439aaa2c4113fae5f9ee6cefa1adc4b0d8df9
vicky-poonia <vickypoonia020@gmail.com>
2023-01-05 12:49:08 +0530

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addCart, delCart } from "../redux/action/index";
-import { useParams } from "react-router";
+// import { useParams } from "react-router";
 import { NavLink } from "react-router-dom";
 
 const Product = () => {
-  const { id } = useParams();
+  // const { id } = useParams();
   const [product, setProduct] = useState([]);
   const [loading, setLoading] = useState(false);
   const [cartBtn, setCartBtn] = useState("Add to Cart");
@@ -24,12 +24,12 @@ const Product = () => {
   useEffect(() => {
     const getProduct = async () => {
       setLoading(true);
-      const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+      const response = await fetch(`https://fakestoreapi.com/products/id`);
       setProduct(await response.json());
       setLoading(false);
     };
     getProduct();
-  }, [id]);
+  }, []);
 
   const Loading = () => {
     return (
